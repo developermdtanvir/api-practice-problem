1a8b0c2dc14e8e2163ac9f1ed7d23989de3e0ff7
devmdtanvir <officialtanvir@hotmail.com>
2023-03-01 09:53:41 +0600

    I  am impliment api recap project seach menu

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # This is my API and ES6 Project Recap
+
+## List of features website
+
+- fetch API
+- Using ES6 features
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// sellact root directory
 const root = document.getElementById("_root");
 
 const url = `https://restcountries.com/v3.1/all`;
@@ -9,13 +10,41 @@ fetch(url)
 const displayAllCountry = (data) => {
   data.map((data) => {
     const div = document.createElement("div");
-    console.log(data);
     root.appendChild(div);
 
     div.classList.add("user-data");
 
     div.innerHTML = `
-            <h1>${data.name.common}</h1>
+            <div class="">
+                <h1 class='text-xl font-bold'>${data.name.common}</h1>
+                <img class="w-20" src=${data.flags.svg} />
+            </div>
         `;
   });
 };
+
+document.getElementById("form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const searchText = document.getElementById("input-search").value;
+  const url = `
+  https://restcountries.com/v3.1/name/${searchText}?fullText=true`;
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.length) {
+        root.style.display = "none";
+      } else {
+        document.getElementById("input-search").classList.add("input-style");
+      }
+      const search_root = document.getElementById("search_root");
+      const div = document.createElement("div");
+      search_root.appendChild(div);
+      div.innerHTML = `
+        <div class=" flex flex-col justify-center items-center">
+            <h1 class='text-xl font-bold'>${data[0].name.common}</h1>
+            <img class=" w-1/2" src=${data[0].flags.svg} />
+        </div>
+    `;
+    });
+});
